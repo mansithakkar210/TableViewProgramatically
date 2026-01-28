c9a918f01cf3608321ef160903eb641d15bed0ae
Mansi Thakkar <mansithakkar210@gmail.com>
2026-01-28 09:10:04 -0500

    Initial Commit

---
+++ TableViewProgramatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewProgramatically
+//
+//  Created by Mansi Thakkar on 2026-01-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
